050174be0d361a476098c285acd9dd08ea821a24
Derek Ruize <derekruiz@usf.edu>
2025-04-12 11:27:10 -0400

    Task 2 - Build TourCard with Conditional rendering and interaction

---
+++ src/components/TourCard.jsx
@@ -0,0 +1,30 @@
+import React,{useState} from "react";
+
+//TourCard renders individual tour details
+const TourCard = ({id, name, info, price, image}) => {
+    //local state to manage the read more/less functionality
+    const [readMore, setReadMore] = useState(false);
+
+    return (
+        <article className="tour-card">
+            <h3>{name}</h3>
+            <h5>{price}</h5>
+
+            {/*Show full description if readMore is true*/}
+            <p>{readMore ? info : `${info.substring(0, 200)}...`}</p>
+            {/*Button to toggle readMore state*/}
+            <button onClick={() => setReadMore(!readMore)}>
+                {readMore ? "Show Less" : "Read More"}
+            </button>
+
+            {/*Button to remove the tour card*/}
+            <button className="delete-btn" onClick={() =>
+                console.log(`Remove tour with id: ${id}`)}>
+                Not Interested
+            </button>
+        </article>
+    )
+}
+
+//Exporting TourCard component
+export default TourCard;
