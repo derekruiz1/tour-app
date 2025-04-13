541cd6d23af4527589fca0731381e265d1df8b5a
Derek Ruize <derekruiz@usf.edu>
2025-04-13 18:00:58 -0400

    Apply Responsive Styling to Tour Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import TourCard from "./components/TourCard";
 import Gallery from "./components/Gallery";
+import "./styles/styles.css";
 
 //Root component of the app
 function App () {
